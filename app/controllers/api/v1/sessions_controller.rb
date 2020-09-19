fbed97d33ebb3b037ebbbc96997759d3e38cbc2c
Vitalii <turzhansky81@gmail.com>
2020-09-19 19:30:18 +0300

    Add Log Out API

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -7,6 +7,8 @@ module Api
 
       before_action :sign_in_params, only: :create
       before_action :load_user, only: :create
+      before_action :valid_token, only: :destroy
+      skip_before_action :verify_signed_out_user, only: :destroy
 
       # sign in
       def create
@@ -18,6 +20,13 @@ module Api
         end
       end
 
+      # log out
+      def destroy
+        sign_out @user
+        @user.generate_new_authentication_token
+        json_response 'Log out Successfully', true, {}, :ok
+      end
+
       private
 
       def sign_in_params
@@ -27,6 +36,11 @@ module Api
       def load_user
         @user = User.find_for_database_authentication(email: sign_in_params[:email])
         @user || json_response('Cannot get User', false, {}, :failure)
+      end
+
+      def valid_token
+        @user = User.find_by authentication_token: request.headers['AUTH-TOKEN']
+        @user || json_response('Invalid Token', false, {}, :failure)
       end
     end
   end
