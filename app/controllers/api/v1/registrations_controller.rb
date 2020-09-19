dee7def5d4c0256ac2399cf2870a7575337e0666
Vitalii <turzhansky81@gmail.com>
2020-09-19 15:34:51 +0300

    Add module Response and test Sign Up with Postman

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,24 +3,16 @@
 module Api
   module V1
     class RegistrationsController < Devise::RegistrationsController
-      before_action :ensure_params_exist, only: :create
+      include Response
 
+      before_action :ensure_params_exist, only: :create
+      # sign up
       def create
-        user = User.new(user_params)
+        user = User.new user_params
         if user.save
-          render json: {
-            messages:   'Signed Up successfully',
-            is_success: true,
-            data:       {
-              user: user
-            }
-          }, status: :ok
+          json_response 'Signed Up successfully', true, { user: user }, :ok
         else
-          render json: {
-            messages:   'Something wrong',
-            is_success: false,
-            data:       {}
-          }, status: :unprocessable_entity
+          json_response 'Something wrong', false, {}, :unprocessable_entity
         end
       end
 
@@ -33,11 +25,7 @@ module Api
       def ensure_params_exist
         return if params[:user].present?
 
-        render json: {
-          messages:   'Missing Params',
-          is_success: false,
-          data:       {}
-        }, status: :bad_request
+        json_response 'Missing Params', false, {}, :bad_request
       end
     end
   end
---
+++ app/controllers/concerns/response.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Response
+  def json_response(messages, is_success, data, status)
+    render json: {
+      messages:   messages,
+      is_success: is_success,
+      data:       data
+    }, status: status
+  end
+end
